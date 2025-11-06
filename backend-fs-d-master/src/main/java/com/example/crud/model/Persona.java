daa9870c5941b94765d2ec4bc7679a5734719a93
Maestro splinter <to.lopezf@duocuc.cl>
2025-11-06 14:55:18 -0300

    Add files via upload

---
+++ backend-fs-d-master/src/main/java/com/example/crud/model/Persona.java
@@ -0,0 +1,22 @@
+package com.example.crud.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Persona {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String nombre;
+
+    //getters y setters id
+    public Long getId(){return id;}
+    public void setId(Long id){this.id=id;}
+
+    //nombre
+    public String getNombre(){return nombre;}
+    public void setNombre(String nombre){this.nombre=nombre;}
+}
